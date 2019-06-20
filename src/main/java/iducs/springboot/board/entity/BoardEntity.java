fe5e24402e2dd86959c63a33d3661b1d5bca5a41
haeri98 <rlagfl9580@naver.com>
2019-06-20 16:01:11 +0900

    cascade설정 질문 지우면 댓글도 같이 사라짐

--- src/main/java/iducs/springboot/board/entity/BoardEntity.java
+++ src/main/java/iducs/springboot/board/entity/BoardEntity.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -32,7 +33,7 @@ public class BoardEntity {
 	@JoinColumn(name="fk_question_writer")
 	private UserEntity writer;	
 	
-	@OneToMany(mappedBy="question")
+	@OneToMany(mappedBy="question", cascade=CascadeType.REMOVE, orphanRemoval = true)
 	@OrderBy("createTime DESC")
 	private List<AnswerEntity> answers = new ArrayList<AnswerEntity>();
 
